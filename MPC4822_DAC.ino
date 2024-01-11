8612359798db882a790778184668fa15ebd041e5
Anders Weijnitz <aweijnitz@Anderss-MacBook-Pro.local>
2024-01-11 11:23:10 +0100

    Docs

--- MPC4822_DAC.ino
+++ MPC4822_DAC.ino
@@ -1,14 +1,25 @@
 
-// Target device: ESP32-C3 Super Mini
-// BOARD in Arduino IDE: ESP32 Dev Board
-// MCP4822 Datasheet https://ww1.microchip.com/downloads/en/DeviceDoc/20002249B.pdf
-// Basic raw example 1: http://www.kerrywong.com/2012/07/25/code-for-mcp4821-mcp4822/
-// Basic raw example 2: https://tahmidmc.blogspot.com/2014/10/pic32-spi-using-mcp4822-12-bit-serial.html
-
-
-// Press and hold both buttons once at start of session to enter sketch load mode
-// Press RST after first upload!
-// Serial.print isn't working for some reason
+/**
+ *
+ * This sketch is a basic blueprint demonstrating how to connect a MCP4822 DAC to an ESP32-C3 Super Mini.
+ *
+ * The code is not optimized for speed, but for portability. It should work on any Arduino or ESp32 with SPI pins.
+ * For increased speed replace digitalWrite(...) with direct port manipulation to toggle the chip select low/high.
+ *
+ * 
+ *
+ * Target device: ESP32-C3 Super Mini
+ * Board to set in Arduino IDE: ESP32 Dev Board
+ * MCP4822 Datasheet https://ww1.microchip.com/downloads/en/DeviceDoc/20002249B.pdf
+ * Basic raw example 1: http://www.kerrywong.com/2012/07/25/code-for-mcp4821-mcp4822/
+ * Basic raw example 2: https://tahmidmc.blogspot.com/2014/10/pic32-spi-using-mcp4822-12-bit-serial.html
+ *
+ *
+ * _Notes:
+ * Press and hold both buttons once at start of session to enter sketch load mode
+ * Press RST after first upload!
+ * Serial.print isn't working for some reason
+*/
 
 #include "SPI.h"
 
